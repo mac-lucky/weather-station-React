2720279abad1abff866ab6b2db01b7033151e711
Maciej Kędziora <maciejkedziora98@gmail.com>
2021-02-18 02:26:01 +0100

    unused var

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -5,7 +5,7 @@ import "./Charts.css";
 
 
 const Charts = (props) => {
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const widthString = Math.round(width * 0.75);
   const api = "https://thingspeak.com/channels/1293688/charts/" + props.field + "?bgcolor=%23282424&color=%23f4c50a&dynamic=true&results=" + props.results +"&type=line&update=15&width=" + widthString + "&days=" + props.days;
 
